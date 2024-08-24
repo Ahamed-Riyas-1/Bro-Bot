2e8e26a9e971c49d847a3507d063f1fa6c506483
Ahamed Riyas <ar.ahamed786@gmail.com>
2024-08-24 22:38:05 +0530

    api change

--- api/index.js
+++ api/index.js
@@ -1,148 +1,66 @@
-import express from 'express';
+import Pact from 'pact-lang-api';
+import TelegramBot from "node-telegram-bot-api";
+import { config } from 'dotenv';
+import cron from 'node-cron';
 
-// pages/api/auto-pump.js
-import 'dotenv/config';
-import { Pact, isSignedTransaction, createSignWithKeypair, createClient } from '@kadena/client';
-import TelegramBot from 'node-telegram-bot-api';
-import Decimal from 'decimal.js';
+// Load environment variables
+config();
 
-// Environment Variables
-const token = process.env.TELEGRAM_BOT_TOKEN || '6954214820:AAHaWt2IH0GWwdGmtLsWOjjg3ukbd-sNN-U';
-const BRO_PUBKEY = process.env.BRO_PUBKEY || '567715f94e4d57581325a4698ad9d1fdc19e30b47478ea72ce54a29a966ef1ac';
-const BRO_PRIVKEY = process.env.BRO_PRIVKEY || 'a0c7356704f44a503c92e03819df73006079d518110f94f86962a0fdfb2edfbe';
-const groupId = process.env.GROUP_ID || -1002198662169;
-const apiHost = process.env.API_HOST || 'https://api.testnet.chainweb.com';
-const network = process.env.NETWORK || 'testnet04';
-const defaultChain = parseInt(process.env.DEFAULT_CHAIN, 10) || 18;
-const GAS_PRICE = process.env.GAS_PRICE || '0.00000001';
-const brons = process.env.BRONS;
-const bro = `${brons}.bro`;
-const bro_treasury = `${brons}.bro-treasury`;
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const TELEGRAM_GROUP_ID = process.env.TELEGRAM_GROUP_ID;
+const NETWORK_ID = process.env.NETWORK_ID
+const API_HOST = process.env.API_HOST
+const PUBLIC_KEY = process.env.PUBLIC_KEY
+const SECRET_KEY = process.env.SECRET_KEY
+const BRO_MAINNET_KEY = process.env.BRO_MAINNET_KEY
+const BRO = `${BRO_MAINNET_KEY}.bro`;
+const BRO_TREASURY = `${BRO_MAINNET_KEY}.bro-treasury`;
 
-// Create bot
-const bot = new TelegramBot(token, { polling: true });
+const KEY_PAIR = {
+    publicKey: PUBLIC_KEY,
+    secretKey: SECRET_KEY,
+};
 
-// Create signer
-const bot_signer = createSignWithKeypair({ publicKey: BRO_PUBKEY, secretKey: BRO_PRIVKEY });
+const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
 
-const getClient = (chain = defaultChain) => createClient(`${apiHost}/chainweb/0.0/${network}/chain/${chain}/pact`);
-
-const app = express();
-
-app.get("/", (req, res) => {
-    res.send("Express on Vercel");
-    do_auto_pump()
+// Schedule the task to run every 1 minutes
+cron.schedule('*/1 * * * *', async () => {
+    try {
+        console.log('Fetching bro price...');
+        await getTokenDetails();
+        console.log('BRO price fetched successfully.');
+    } catch (error) {
+        console.error('Error running task:', error);
+    }
 });
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+const creationTime = () => Math.round(new Date().getTime() / 1000);
 
-// Define your auto-pump function
-export async function do_auto_pump() {
-    const gatherable_rewards = await gatherableRewards();
-    console.log(`Rewards available: ${gatherable_rewards}`);
+async function getTokenDetails(accountPublicKey) {
+    const cmd = {
+        networkId: NETWORK_ID,
+        keyPairs: [KEY_PAIR],
+        pactCode: `(let ((acct (${BRO_TREASURY}.dex-account)))
+                 (round (/ (coin.get-balance acct)
+                           (${BRO}.get-balance acct))
+                        2))`,
+        envData: {},
+        meta: {
+            creationTime: creationTime(),
+            ttl: 600,
+            gasLimit: 150000,
+            chainId: '18',
+            gasPrice: 0.0000001,
+            sender: KEY_PAIR.publicKey,
+        },
+    };
 
-    if (gatherable_rewards && gatherable_rewards.gte(Decimal('0.01'))) {
-        console.log('Auto pumping');
-        const msg = await bot.sendMessage(groupId, 'Auto-pumping $BRO in progress');
-        const requestKey = await gather_rewards();
-        const statusResult = await status(requestKey);
-
-        if (statusResult?.result?.status === 'success') {
-            await msg.edit('Auto-pump successful');
-        } else {
-            await msg.edit('Auto-pump error');
-        }
-
-        setTimeout(() => msg.delete({ revoke: true }), 3600_000);
-        const price = await getBroPrice();
-        const priceMessage = `New price: ${price.toString()} KDA / $BRO`;
-        const priceMsg = await bot.sendMessage(groupId, priceMessage);
-        setTimeout(() => priceMsg.delete({ revoke: true }), 3600_000);
-    } else {
-        console.log('Not enough rewards to gather => Cancel');
+    try {
+        const result = await Pact.fetch.local(cmd, API_HOST);
+        const broPrice = result.result.data;
+        bot.sendMessage(TELEGRAM_GROUP_ID, `Bro Price : ${broPrice} KDA`);
+        console.log('Token Details:', result);
+    } catch (error) {
+        console.error('Error fetching token details:', error);
     }
 }
-
-const gatherableRewards = async () => {
-    const code = `(${bro_treasury}.liquidity-to-remove)`;
-    const response = await pactCalls(code, defaultChain);
-    return parsePactResponseThrow(response);
-};
-
-const gather_rewards = async () => {
-    const jsclient = getClient();
-    const unsignedTransaction = Pact.builder
-        .execution(`(${brons}.bro-treasury.gather-rewards)`)
-        .setMeta({
-            chainId: String(defaultChain),
-            senderAccount: `r:${brons}.bot`,
-            gasLimit: 12000,
-            gasPrice: GAS_PRICE,
-            ttl: 14400,
-        })
-        .addSigner(BRO_PUBKEY, signFor => [
-            signFor('coin.GAS'),
-            signFor(`${brons}.bro-treasury.OPERATE-DEX`),
-        ])
-        .setNetworkId(network)
-        .createTransaction();
-    const signedTx = await bot_signer(unsignedTransaction);
-
-    const preflightResult = await jsclient.preflight(signedTx);
-    if (preflightResult.result.status === 'failure') {
-        throw new Error(preflightResult.result.error.message);
-    }
-
-    if (isSignedTransaction(signedTx)) {
-        const transactionDescriptor = await jsclient.submit(signedTx);
-        return transactionDescriptor.requestKey;
-    }
-};
-
-const status = async (hash) => {
-    const jsclient = getClient();
-    const result = await jsclient.pollStatus(
-        { requestKey: hash, chainId: defaultChain, networkId: network },
-        { timeout: 240_000, interval: 5000 }
-    );
-    return result?.[hash];
-};
-
-const getBroPrice = async () => {
-    const code = `(let ((acct (${bro_treasury}.dex-account)))
-                 (round (/ (coin.get-balance acct)
-                           (${bro}.get-balance acct))
-                        2))`;
-    const response = await pactCalls(code, defaultChain);
-    return parsePactResponseThrow(response);
-};
-
-const parsePactResponseThrow = (response) => {
-    try {
-        if (response?.result?.status === 'success') {
-            return Decimal(response.result.data);
-        }
-    } catch (e) {
-        console.log(response?.result?.error?.message);
-    }
-};
-
-const pactCalls = async (code, chain) => {
-    const pactClient = getClient(chain);
-    const tx = Pact.builder
-        .execution(code)
-        .setMeta({
-            chainId: String(chain),
-            gasLimit: 100000,
-            gasPrice: 0.0000001,
-        })
-        .setNetworkId(network)
-        .createTransaction();
-
-    try {
-        return await pactClient.dirtyRead(tx);
-    } catch (error) {
-        console.error('Error fetching account details:', error);
-        throw error;
-    }
-};
