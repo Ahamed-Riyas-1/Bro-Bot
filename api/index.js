452e387acada56e0d9dfc30f6c209fe487796865
Ahamed Riyas <ar.ahamed786@gmail.com>
2024-08-24 22:53:43 +0530

    api change

--- api/index.js
+++ api/index.js
@@ -1,18 +1,17 @@
-import Pact from 'pact-lang-api';
-import TelegramBot from "node-telegram-bot-api";
 import { config } from 'dotenv';
-import cron from 'node-cron';
+import TelegramBot from 'node-telegram-bot-api';
+import Pact from 'pact-lang-api';
 
 // Load environment variables
 config();
 
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 const TELEGRAM_GROUP_ID = process.env.TELEGRAM_GROUP_ID;
-const NETWORK_ID = process.env.NETWORK_ID
-const API_HOST = process.env.API_HOST
-const PUBLIC_KEY = process.env.PUBLIC_KEY
-const SECRET_KEY = process.env.SECRET_KEY
-const BRO_MAINNET_KEY = process.env.BRO_MAINNET_KEY
+const NETWORK_ID = process.env.NETWORK_ID;
+const API_HOST = process.env.API_HOST;
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
+const SECRET_KEY = process.env.SECRET_KEY;
+const BRO_MAINNET_KEY = process.env.BRO_MAINNET_KEY;
 const BRO = `${BRO_MAINNET_KEY}.bro`;
 const BRO_TREASURY = `${BRO_MAINNET_KEY}.bro-treasury`;
 
@@ -21,22 +20,11 @@ const KEY_PAIR = {
     secretKey: SECRET_KEY,
 };
 
-const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
-
-// Schedule the task to run every 1 minutes
-cron.schedule('*/1 * * * *', async () => {
-    try {
-        console.log('Fetching bro price...');
-        await getTokenDetails();
-        console.log('BRO price fetched successfully.');
-    } catch (error) {
-        console.error('Error running task:', error);
-    }
-});
+const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: false });
 
 const creationTime = () => Math.round(new Date().getTime() / 1000);
 
-async function getTokenDetails(accountPublicKey) {
+async function getTokenDetails() {
     const cmd = {
         networkId: NETWORK_ID,
         keyPairs: [KEY_PAIR],
@@ -58,9 +46,20 @@ async function getTokenDetails(accountPublicKey) {
     try {
         const result = await Pact.fetch.local(cmd, API_HOST);
         const broPrice = result.result.data;
-        bot.sendMessage(TELEGRAM_GROUP_ID, `Bro Price : ${broPrice} KDA`);
+        await bot.sendMessage(TELEGRAM_GROUP_ID, `Bro Price: ${broPrice} KDA`);
         console.log('Token Details:', result);
     } catch (error) {
         console.error('Error fetching token details:', error);
+    }
+}
+
+export default async function handler(req, res) {
+    console.log(req);
+    try {
+        await getTokenDetails();
+        res.status(200).send('BRO price fetched and sent successfully.');
+    } catch (error) {
+        console.error('Error in handler:', error);
+        res.status(500).send('Failed to fetch and send BRO price.');
     }
 }
