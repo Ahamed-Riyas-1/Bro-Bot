811ebf267e859b7498fdeef169e61cd26db15b48
Ahamed Riyas <ar.ahamed786@gmail.com>
2024-08-25 14:40:04 +0530

    scheduler change

--- netlify/functions/bro-price.js
+++ netlify/functions/bro-price.js
@@ -49,27 +49,10 @@ async function getTokenDetails() {
     }
 }
 
-exports.handler = async function(event, context) {
-    console.log('Environment Variables:', {
-        TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
-        TELEGRAM_GROUP_ID: process.env.TELEGRAM_GROUP_ID,
-        NETWORK_ID: process.env.NETWORK_ID,
-        API_HOST: process.env.API_HOST,
-        PUBLIC_KEY: process.env.PUBLIC_KEY,
-        SECRET_KEY: process.env.SECRET_KEY,
-        BRO_MAINNET_KEY: process.env.BRO_MAINNET_KEY,
-    });
+exports.handler = async function() {
     await getTokenDetails();
     return {
         statusCode: 200,
-        body: JSON.stringify({
-        TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
-        TELEGRAM_GROUP_ID: process.env.TELEGRAM_GROUP_ID,
-        NETWORK_ID: process.env.NETWORK_ID,
-        API_HOST: process.env.API_HOST,
-        PUBLIC_KEY: process.env.PUBLIC_KEY,
-        SECRET_KEY: process.env.SECRET_KEY,
-        BRO_MAINNET_KEY: process.env.BRO_MAINNET_KEY,
-    }),
+        body: JSON.stringify({ message: 'BRO price fetched successfully.' }),
     };
 };
