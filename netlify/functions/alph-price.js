8523daa821eac4f49c6ca7b5235c8eec872308d6
Ahamed Riyas <ar.ahamed786@gmail.com>
2024-09-11 21:16:03 +0530

    alph price change

--- netlify/functions/alph-price.js
+++ netlify/functions/alph-price.js
@@ -37,7 +37,7 @@ exports.handler = async function () {
 
         return {
             statusCode: 200,
-            body: JSON.stringify({ message: 'Telegram message sent successfully', data: filteredData }),
+            body: JSON.stringify({ message: message}),
         };
     } catch (error) {
         console.error(error);
